a9ea8efe4f7d479770d8514b8747bbfd4913b1b2
ansshour <andrey.zhukov001@gmail.com>
2022-07-06 22:03:37 +0300

    with slider

--- src/components/UI/Slider/Slider.tsx
+++ src/components/UI/Slider/Slider.tsx
@@ -1,52 +1,65 @@
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import "./Slider.css"
+import { useState } from "react";
 import styles from "./Slider.module.css"
 
-type dataType = {
+
+type Props = {
     image: string;
     text: string[];
     link: string;
+    linkName: string;
 }
 
-type Props = {
-    data: dataType[];
-}
+export const Slider: React.FC<any> = ({ data }) => {
 
-export const Slider: React.FC<Props> = ({ data }) => {
+    const [elementOffset, setElementOffset] = useState(0);
+    let [activeElement, setActiveElement] = useState(0);
+
+
+    const nextHundler = () => {
+        if (elementOffset > -(data.length - 1) * 100) {
+            setElementOffset(elementOffset - 100)
+            setActiveElement(activeElement + 1)
+        } else {
+            setElementOffset(0)
+            setActiveElement(0)
+        }
+    }
+
+    const prevHundler = () => {
+        if (elementOffset < 0) {
+            setElementOffset(elementOffset + 100)
+            setActiveElement(activeElement - 1)
+        } else {
+            setElementOffset(-(data.length - 1) * 100)
+            setActiveElement(data.length - 1)
+        }
+    }
+
+
     return (
-        <Swiper
-            // install Swiper modules
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={50}
-            slidesPerView={1}
-            navigation
-            pagination={{ clickable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-        >
-            {data.map(({ image, text, link }) => (
-                <SwiperSlide>
-                    <div className={styles.container}>
-                        <div className={styles.image} style={{ backgroundImage: `url(${image})` }}>
-
-                        </div>
-                        <div className={styles.descriptions}>
-                            {text.map((text) => (
-                                <p className={styles.desc}>{text}</p>
-                            ))}
-                        </div>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+        <div className={styles.container}>
+            <div className={styles.prev} onClick={prevHundler}></div>
+            <div className={styles.slider}>
+                <div className={styles.activeElement} style={{ transform: `translateX(${elementOffset}%)` }}>
+                    {data.map((elem: Props) => {
+                        return (
+                            <div className={styles.slide}>
+                                <div className={styles.image} style={{ backgroundImage: `url(${elem.image})` }}></div>
+                                <div className={styles.text}>
+                                    {elem.text.map((text: string) => <p key={text}>{text}</p>)}
+                                    <a className={styles.link} href={elem.link}>{elem.linkName}</a>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+            <div className={styles.next} onClick={nextHundler}></div>
+            <div className={styles.points}>
+                {data.map((elem: Props, id: number) => (
+                    id === activeElement ? <div className={styles.point} style={{ backgroundColor: "black" }} key={id}></div> : <div className={styles.point} key={id}></div>
+                ))}
+            </div>
+        </div >
     )
 }
