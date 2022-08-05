b8a2d9886a500589ee5221e94bb1d5e18a7b3cdd
ansshour <andrey.zhukov001@gmail.com>
2022-08-05 17:26:10 +0300

    feat: connected api to publications

--- src/components/Publications/Publications.tsx
+++ src/components/Publications/Publications.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, createContext } from "react"
+import { useState, useRef, createContext, useEffect } from "react"
 import { Breadcrumbs } from "../UI/Breadcrumbs/Breadcrumbs"
 import { Button } from "../UI/Button/Button"
 import { SearchInput } from "../UI/SearchInput/SearchInput"
@@ -30,119 +30,112 @@ export const Publications = () => {
         images: ["./images/covers/1.svg", "./images/covers/2.svg", "./images/covers/3.svg", "./images/covers/4.svg", "./images/covers/5.svg", ""],
     }
 
-    const modalData = {
-        gost: "Шалагина Г. Э., Шалагин С. В. Компьютерное моделирование в инфохимии: экологические вызовы и новые технологии //Устойчивое развитие регионов: опыт, проблемы, перспективы. – 2017. – С. 169-172.",
-        mla: "Шалагина Г. Э., Шалагин С. В. Компьютерное моделирование в инфохимии: экологические вызовы и новые технологии //Устойчивое развитие регионов: опыт, проблемы, перспективы. – 2017. – С. 169-172.",
-        apa: "Шалагина Г. Э., Шалагин С. В. Компьютерное моделирование в инфохимии: экологические вызовы и новые технологии //Устойчивое развитие регионов: опыт, проблемы, перспективы. – 2017. – С. 169-172.",
+    const [publicationsInfo, setPublicationsInfo] = useState<any>([]);
+
+    const getInfo = async () => {
+        const data = await fetch("http://78.140.243.10/api/publications/stats/");
+        const response = await data.json();
+        setPublicationsInfo(response)
+
     }
 
-    const quoteData = [
-        {
-            authors: "Shityakov S. Skorb E.V Forster C. Dankdekar T",
-            name: "Scaffold searching of FDA and EMA-approved drugs identifies lead candidates for drug repurposing in Alzheimer's disease",
-            place: "Frontiers in Chemistry, 2021",
-            code: "[ IF: 10.041 , SJR: 2.287 ]",
-        },
-        {
-            authors: "Shityakov S. Skorb E.V Forster C. Dankdekar T",
-            name: "Scaffold searching of FDA and EMA-approved drugs identifies lead candidates for drug repurposing in Alzheimer's disease",
-            place: "Frontiers in Chemistry, 2021",
-            code: "[ IF: 10.041 , SJR: 2.287 ]",
-        },
-        {
-            authors: "Shityakov S. Skorb E.V Forster C. Dankdekar T",
-            name: "Scaffold searching of FDA and EMA-approved drugs identifies lead candidates for drug repurposing in Alzheimer's disease",
-            place: "Frontiers in Chemistry, 2021",
-            code: "[ IF: 10.041 , SJR: 2.287 ]",
-        },
-        {
-            authors: "Shityakov S. Skorb E.V Forster C. Dankdekar T",
-            name: "Scaffold searching of FDA and EMA-approved drugs identifies lead candidates for drug repurposing in Alzheimer's disease",
-            place: "Frontiers in Chemistry, 2021",
-            code: "[ IF: 10.041 , SJR: 2.287 ]",
-        },
-        {
-            authors: "Shityakov S. Skorb E.V Forster C. Dankdekar T",
-            name: "Scaffold searching of FDA and EMA-approved drugs identifies lead candidates for drug repurposing in Alzheimer's disease",
-            place: "Frontiers in Chemistry, 2021",
-            code: "[ IF: 10.041 , SJR: 2.287 ]",
-        },
-        {
-            authors: "Shityakov S. Skorb E.V Forster C. Dankdekar T",
-            name: "Scaffold searching of FDA and EMA-approved drugs identifies lead candidates for drug repurposing in Alzheimer's disease",
-            place: "Frontiers in Chemistry, 2021",
-            code: "[ IF: 10.041 , SJR: 2.287 ]",
-        },
-    ]
+
+    const [publicationsData, setPublicationsData] = useState<any>([])
+
+    const getPublicationsData = async () => {
+        const data = await fetch(`http://78.140.243.10/api/publications/papers/?page=1&page_size=10000000`);
+        const { results } = await data.json();
+        setPublicationsData(results)
+    }
+
+
+    useEffect(() => {
+        getPublicationsData()
+        getInfo()
+    }, [])
 
     const slideCovers = (direction: "left" | "right") => {
         let width = ref.current.offsetWidth + 35;
         const intoScreen = Math.round(refContainer.current.offsetWidth / width);
-        console.log(refContainer.current.offsetWidth / width, " ", width)
         if (direction === "left") {
             if (coverOffset < 0) {
                 setCoverOffset(coverOffset + width);
             }
         }
         if (direction === "right") {
-            console.log(-(data.images.length - intoScreen) * width)
             if (coverOffset > -(data.images.length - intoScreen) * width) {
                 setCoverOffset(coverOffset - width);
             }
         }
     }
 
+    const dataSortByDate = (data: any) => {
+        const publications: any = {};
+        const result = [];
+        data.forEach((elem: any) => {
+            if (publications[elem.year]) {
+                publications[elem.year] = [...publications[elem.year], elem]
+            } else {
+                publications[elem.year] = [];
+                publications[elem.year] = [...publications[elem.year], elem]
+            }
+        })
+        for (const key in publications) {
+            result.push(publications[key])
+        }
+        return result
+    }
+
+
     return (
         <>
-            <PublicationsContext.Provider value={{ quoteData, modalData }} >
-                <Breadcrumbs info={info} />
-                <div className={styles.container} ref={refContainer}>
-                    <div className={styles.title__wrapper}>
-                        <h1 className={styles.title}>
-                            Публикации
-                        </h1>
+            <Breadcrumbs info={info} />
+            <div className={styles.container} ref={refContainer}>
+                <div className={styles.title__wrapper}>
+                    <h1 className={styles.title}>
+                        Публикации
+                    </h1>
+                </div>
+                <div className={styles.searchBlock}>
+                    <SearchInput />
+                    <Button height="35" >Поиск</Button>
+                </div>
+                <div className={styles.numbersBlock}>
+                    <div className={styles.block}>
+                        <p className={styles.blockTitle}>Всего</p>
+                        <p className={styles.digit}>{publicationsInfo.paper_count}</p>
                     </div>
-                    <div className={styles.searchBlock}>
-                        <SearchInput />
-                        <Button height="35" >Поиск</Button>
+                    <div className={styles.block}>
+                        <p className={styles.blockTitle}>Издательств</p>
+                        <p className={styles.digit}>{publicationsInfo.journals_count}</p>
                     </div>
-                    <div className={styles.numbersBlock}>
-                        <div className={styles.block}>
-                            <p className={styles.blockTitle}>Всего</p>
-                            <p className={styles.digit}>{data.inTotal}</p>
-                        </div>
-                        <div className={styles.block}>
-                            <p className={styles.blockTitle}>Издательств</p>
-                            <p className={styles.digit}>{data.publishing}</p>
-                        </div>
-                        <div className={styles.block}>
-                            <p className={styles.blockTitle}>Авторов</p>
-                            <p className={styles.digit}>{data.authors}</p>
-                        </div>
-                        <div className={styles.block}>
-                            <p className={styles.blockTitle}>За лет</p>
-                            <p className={styles.digit}>{data.totalYears}</p>
-                        </div>
+                    <div className={styles.block}>
+                        <p className={styles.blockTitle}>Авторов</p>
+                        <p className={styles.digit}>{publicationsInfo.authors_count}</p>
                     </div>
-                    <p className={styles.subtitle}>Публикуются в изданиях:</p>
-                    <div className={styles.coversWrapper}>
-                        <div className={styles.covers} style={{ transform: `translateX(${coverOffset}px)` }}>
-                            {data.images.map((image: string, i: number) => <div key={i} className={styles.cover} style={{ backgroundImage: `url(${image})` }} ref={ref}></div>)}
-                        </div>
-                    </div>
-
-                    <div className={styles.navCoversWrapp}>
-                        <div className={styles.navigate}>
-                            <Left className={styles.left} onClick={() => { slideCovers("left") }} />
-                            <Right className={styles.right} onClick={() => { slideCovers("right") }} />
-                        </div>
-                    </div>
-                    <p className={styles.subtitle}>Публикации</p>
-                    <div className={styles.quotesWrapper}>
-                        <Quotes />
+                    <div className={styles.block}>
+                        <p className={styles.blockTitle}>За лет</p>
+                        <p className={styles.digit}>{publicationsInfo.years_count}</p>
                     </div>
                 </div>
-            </PublicationsContext.Provider>
+                <p className={styles.subtitle}>Публикуются в изданиях:</p>
+                <div className={styles.coversWrapper}>
+                    <div className={styles.covers} style={{ transform: `translateX(${coverOffset}px)` }}>
+                        {publicationsData.map(({ journal_cover }: any, i: number) => <div key={i} className={styles.cover} style={{ backgroundImage: `url(${journal_cover})` }} ref={ref}></div>)}
+                    </div>
+                </div>
+
+                <div className={styles.navCoversWrapp}>
+                    <div className={styles.navigate}>
+                        <button className={styles.navigateBtns}><Left className={styles.left} onClick={() => { slideCovers("left") }} /></button>
+                        <button className={styles.navigateBtns}><Right className={styles.right} onClick={() => { slideCovers("right") }} /></button>
+                    </div>
+                </div>
+                <p className={styles.subtitle}>Публикации</p>
+                <div className={styles.quotesWrapper}>
+                    {dataSortByDate(publicationsData).reverse().map((item: any) => <Quotes data={item} />)}
+                </div>
+            </div>
         </>
     )
 }
--- src/components/ScienceGroup/ScienceGroupTable/ScienceGroupTable.tsx
+++ src/components/ScienceGroup/ScienceGroupTable/ScienceGroupTable.tsx
@@ -3,6 +3,7 @@ import styles from "./ScienceGroupTable.module.css"
 type Area = {
     title: string;
     desc: string;
+    id: number;
 }
 
 type Props = {
@@ -13,13 +14,19 @@ type Props = {
     tasks: string[];
     area: Area[];
     icon: string;
+    id: number;
 }
 
 const classnames = [styles.first, styles.second, styles.third, styles.fourth]
 
 export const ScienceGroupTable: React.FC<Props> = ({ groupName, photo, name, email, tasks, area, icon }) => {
+
+
     return (
-        <div className={styles.container}>
+        <div className={styles.container}
+            style={area.length === 3 ? {
+                gridTemplateAreas: ` "main first second" "main third third" `
+            } : {}}>
             <div className={styles.main}>
                 <img className={styles.icon} src={icon} />
                 <p className={styles.groupName}>{groupName}</p>
@@ -34,12 +41,12 @@ export const ScienceGroupTable: React.FC<Props> = ({ groupName, photo, name, ema
                     </div>
                 </div>
                 <ul className={styles.tasksList}>
-                    {tasks.map((task) => <li>{task}</li>)}
+                    {tasks.map(({ name, id }: any) => <li key={`${name} ${id}`}>{name}</li>)}
                 </ul>
             </div>
-            {area.map(({ title, desc }: Area, i: number) => {
+            {area.map(({ title, desc, id }: Area, i: number) => {
                 return (
-                    <div className={[styles.area, classnames[i]].join(" ")}>
+                    <div className={[styles.area, classnames[i]].join(" ")} key={`${title} ${desc}`}>
                         <p className={styles.title}>{title}</p>
                         <p className={styles.desc}>{desc}</p>
                     </div>
